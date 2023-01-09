d46c32f160016a4c55df76c98c3129953deede9b
SiddiquiUbaid <73527547+SiddiquiUbaid@users.noreply.github.com>
2023-01-09 17:59:12 +0530

    2 files modified, 1 deleted

--- app/main-page.js
+++ app/main-page.js
@@ -1,6 +1,17 @@
-import { createViewModel } from './main-view-model';
+function onTap(args) {
+  const obj = args.object;
+  const page = obj.page;
 
-export function onNavigatingTo(args) {
-  const page = args.object;
-  page.bindingContext = createViewModel();
+  
+   console.log('trying to open home page ')
+   const  homePage = {moduleName: 'views/home/home',
+                      transition: { 
+                        name: "explode" //explode //fade //flipRight //flipLeft //slideLeft //slideRight //slideTop //slideBottom 
+                      }
+                     }
+   page.frame.navigate(homePage)
+  
+  // page.frame.navigate('main-page'); //saregampadhani
+
 }
+exports.onTap = onTap;
